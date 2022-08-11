9926c2f5baebe2ef66a3c820f54e995e792dd19e
CoolD69 <parthasingharoy166@gmail.com>
2022-08-11 11:17:57 +0530

    update news

--- src/components/News.js
+++ src/components/News.js
@@ -4,20 +4,20 @@ import PropTypes from 'prop-types'
 
 let API = "554578280c364851bc7b40b4accf332c";
 export class News extends Component {
-  
-    static defaultProps={
+
+    static defaultProps = {
         country: 'in',
         pageSize: 9,
         category: 'general'
     }
-     Proptypes={
+    Proptypes = {
         country: PropTypes.string,
         pageSize: PropTypes.number,
         category: PropTypes.string,
     }
 
     articles = []
-    
+
     constructor() {
         super();
         this.state = {
@@ -29,12 +29,12 @@ export class News extends Component {
     }
     async componentDidMount() {
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&from=2022-08-11&sortBy=popularity&apiKey=${API}&pageSize=${this.pageSize}`
-        
+
         let data = await fetch(url)
         let dataParse = await data.json()
         this.setState({ articles: dataParse.articles })
     }
-    
+
     previousClick = async () => {
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&from=2022-08-11&sortBy=popularity&apiKey=${API}&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`
         let data = await fetch(url)
@@ -44,10 +44,10 @@ export class News extends Component {
             articles: dataParse.articles,
         })
     }
-    
+
     nextClick = async () => {
-        if (this.state.page + 1 > Math.ceil(this.state.totalResults /this.pageSize)) {
-            
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / this.pageSize)) {
+
         }
         else {
             let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&from=2022-08-11&sortBy=popularity&apiKey=${API}&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`
@@ -58,29 +58,29 @@ export class News extends Component {
                 articles: dataParse.articles,
             })
         }
-        
+
     }
-    
+
     default_url = 'https://st1.bgr.in/wp-content/uploads/2021/12/Flipkart-Smartphone-Deals.jpg'
     render() {
-     
+
         return (
             <>
-            <div className='container my-3'>
-                <h2>News Api - Headline</h2>
-                <div className='row'>
-                    {this.state.articles.map((element) => {
-                        return <div key={element.url} className='col-md-4'>
-                            <Newsitems title={element.title.slice(0, 40)} descriptions={element.description ? element.description.slice(0, 70) : ''} imageUrl={element.urlToImage ? element.urlToImage : this.default_url} newsUrl={element.url} author={element.author} date={element.publisheAt} />
-                        </div>
-                    })}
-                </div>
+                <div className='container my-3'>
+                    <h2>News Api - Headline</h2>
+                    <div className='row'>
+                        {this.state.articles.map((element) => {
+                            return <div key={element.url} className='col-md-4'>
+                                <Newsitems title={element.title.slice(0, 40)} descriptions={element.description ? element.description.slice(0, 70) : ''} imageUrl={element.urlToImage ? element.urlToImage : this.default_url} newsUrl={element.url} author={element.author} date={element.publisheAt} />
+                            </div>
+                        })}
+                    </div>
 
-                <div className='container d-flex justify-content-between'>
-                    <button disabled={this.state.page <= 1} type="button" onClick={this.previousClick} className="btn btn-warning"> &larr; Previous</button>
-                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)} type="button" onClick={this.nextClick} className="btn btn-warning">Next &rarr; </button>
+                    <div className='container d-flex justify-content-between'>
+                        <button disabled={this.state.page <= 1} type="button" onClick={this.previousClick} className="btn btn-warning"> &larr; Previous</button>
+                        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / 20)} type="button" onClick={this.nextClick} className="btn btn-warning">Next &rarr; </button>
+                    </div>
                 </div>
-            </div>
             </>
         )
     }
