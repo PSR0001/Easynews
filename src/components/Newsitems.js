f68525bad6a1b2be4aaa7278e28840eb71b541b6
CoolD69 <parthasingharoy166@gmail.com>
2022-08-11 11:16:57 +0530

    update newsitem

--- src/components/Newsitems.js
+++ src/components/Newsitems.js
@@ -2,19 +2,18 @@ import React, { Component } from 'react'
 
 export class Newsitems extends Component {
     render() {
-        let {title , descriptions,imageUrl, newsUrl, author, date} = this.props;
+        let { title, descriptions, imageUrl, newsUrl, author, date } = this.props;
         return (
             <div className='my-3'>
-                <div className="card" style = {{'width': '18rem'}} >
-                    <img src={imageUrl} className="card-img-top" alt="..."/>
-                        <div className="card-body">
-                            <h5 className="card-title">{title}</h5>
-                            <p className="card-text">{descriptions}</p>
-                            <p className="card-text"><small className="text-muted">By {author?author:"Unkhown"} on {new Date(date).toGMTString()}</small></p>
-                            <a href={newsUrl} target="_blank" rel='noreferrer' className="btn btn-sm btn-primary">Read Mode</a>
-                        </div>
+                <div className="card" style={{ 'width': '18rem' }} >
+                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{descriptions}</p>
+                        <p className="card-text"><small className="text-muted">By {author ? author : "Unkhown"} on {new Date(date).toGMTString()}</small></p>
+                        <a href={newsUrl} target="_blank" rel='noreferrer' className="btn btn-sm btn-primary">Read More</a>
+                    </div>
                 </div>
-                
             </div>
         )
     }
